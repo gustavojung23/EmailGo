ac34af1b1476251bb73122856da4595b234022a4
Gustavo Jung <gujung23@gmail.com>
2024-05-20 23:16:49 -0300

    Update campaing.go - created func NewCampaign

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -13,3 +13,20 @@ type Campaign struct {
 	Content   string
 	Contacts  []Contact
 }
+
+func NewCampaign(name string, content string, emails []string) *Campaign {
+
+	contacts := make([]Contact, len(emails))
+
+	for index, email := range emails {
+		contacts[index].Email = email
+	}
+
+	return &Campaign{
+		ID:        "1",
+		Name:      name,
+		CreatedOn: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}
+}
