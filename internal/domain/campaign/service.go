9f355ca972726d02eefa46d9d50d77964345f5bb
Gustavo Jung <gujung23@gmail.com>
2024-05-30 19:44:57 -0300

    Update service then internalerrors

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"emailgo/internal/contract"
+	internalerrors "emailgo/internal/internal-errors"
 )
 
 type Service struct {
@@ -10,15 +11,13 @@ type Service struct {
 
 func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
 	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
-
 	if err != nil {
 		return "", err
 	}
 
 	err = s.Repository.Save(campaign)
-
 	if err != nil {
-		return "", err
+		return "", internalerrors.ErrInternal
 	}
 
 	return campaign.ID, nil
