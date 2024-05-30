016d4128422c1e85f71136590bdb2d830928dc78
Gustavo Jung <gujung23@gmail.com>
2024-05-30 19:42:52 -0300

    create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"emailgo/internal/domain/campaign"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func main() {
+	contacts := []campaign.Contact{{Email: "gustavo@teste.com.br"}, {Email: ""}}
+	campaign := campaign.Campaign{Contacts: contacts}
+	validate := validator.New()
+	err := validate.Struct(campaign)
+	if err == nil {
+		println("Nenhum Erro")
+	} else {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, v := range validationErrors {
+			switch v.Tag() {
+			case "required":
+				println(v.StructField() + " is required")
+			case "min":
+				println(v.StructField() + " is required with min " + v.Param())
+			case "max":
+				println(v.StructField() + " is required with max " + v.Param())
+			case "email":
+				println(v.StructField() + " is invalid " + v.Param())
+			}
+		}
+	}
+}
