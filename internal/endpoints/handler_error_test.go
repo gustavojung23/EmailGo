d61e1a0fc37d4f3e0531bbb9dedad91db0c781da
Gustavo Jung <gujung23@gmail.com>
2024-07-17 18:54:03 -0300

    Test Handler Internal Error and Domain Error

---
+++ internal/endpoints/handler_error_test.go
@@ -0,0 +1,43 @@
+package endpoints
+
+import (
+	internalerrors "emailgo/internal/internal-errors"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internalerrors.ErrInternal
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), internalerrors.ErrInternal.Error())
+}
+
+func Test_HandlerError_when_endpoint_returns_domain_error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, errors.New("Domain error")
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+	assert.Contains(res.Body.String(), "Domain error")
+}
