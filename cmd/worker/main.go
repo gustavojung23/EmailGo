353c26b188402ff1e9d77368f5061c78fe58eb32
Gustavo Jung <gujung23@gmail.com>
2024-07-29 14:37:40 -0300

    .env update path

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Started Worker")
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
